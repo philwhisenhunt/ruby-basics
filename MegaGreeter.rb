0496d2f9c12747831b03ed7257be347327d13c5d
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-22 22:53:59 -0500

    added a bit more but need to type out understanding for each line

--- MegaGreeter.rb
+++ MegaGreeter.rb
@@ -1,4 +1,9 @@
+#anything after this symbol on the same line will be a comment
+
 class MegaGreeter
     attr_accessor :names
 
     # Create the object
+    def initialize(names = "World")
+        @names = names
+    end
