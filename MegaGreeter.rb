02c9469b3592d1126dcfb01212a89d2993c23ef9
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-23 22:14:13 -0500

    Building out more from example

--- MegaGreeter.rb
+++ MegaGreeter.rb
@@ -7,3 +7,11 @@ class MegaGreeter
     def initialize(names = "World")
         @names = names
     end
+
+    #say hi to everyone
+    def say_hi
+        if @names.nil?
+            puts "..."
+        else if @names.respond_to("each")
+            # @names is a list of some kind, iterate!
+            @names.each do |name|
