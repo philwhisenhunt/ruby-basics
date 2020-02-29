d006651aa2e1bef521e3f158017c64c01c4b045b
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-28 22:18:46 -0500

    Building out introductions

--- starter.rb
+++ starter.rb
@@ -1,3 +1,6 @@
 print "Type your name"
 first_name = gets.chomp
 print "And what is your last name, " + first_name + " ?"
+
+last_name = gets.chomp
+print "Thanks " + first_name + " " + last_name ", now tell us how old you are"
