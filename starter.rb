e467b49d298b582e6a72563b959dec34f3fca127
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-02-29 23:36:33 -0500

    Building out questions

--- starter.rb
+++ starter.rb
@@ -4,3 +4,6 @@ print "And what is your last name, " + first_name + " ?"
 
 last_name = gets.chomp
 print "Thanks " + first_name + " " + last_name ", now tell us how old you are"
+
+age = gets.chomp
+print "One more questions, what state are you from?"
