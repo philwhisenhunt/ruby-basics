e590d29fbcf41dada8a3e3f9d9939199bd1e309d
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-03-01 21:55:59 -0500

    Learning to put

--- starter.rb
+++ starter.rb
@@ -7,3 +7,6 @@ print "Thanks " + first_name + " " + last_name ", now tell us how old you are"
 
 age = gets.chomp
 print "One more questions, what state are you from?"
+
+puts "Thanks!"
+print "There should be a line down here"
