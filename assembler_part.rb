794f6e61b5fc59431bb2bae89747408f18d53409
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-03-22 23:17:20 -0400

    Buidling out Assembler_part class

--- assembler_part.rb
+++ assembler_part.rb
@@ -1,7 +1,14 @@
 require 'assembler.rb'
 
 class Assembler_part < Assembler
+
+    def initialize(input1, input2) do
+        puts "initializing"
+    end
+
     def assembler(input1, input2) do
         assembled = input.combine(input1, input2)
     end
 end
+
+handle = Assembler_part.new
