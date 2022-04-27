c442792191fef5182617a204e5cd557a04019e3d
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-04-27 19:09:17 -0400

    Pays to read the instructions

--- dup_finder.rb
+++ dup_finder.rb
@@ -4,9 +4,10 @@ def contains_duplicate(nums)
     tried_nums = []
     nums.each do |num|
         if tried_nums.include?(num) 
-            return false
+            return true
         else
             tried_nums << num 
         end
     end
+    return false
 end
