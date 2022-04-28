a5d561c29bc3f030bf1555bb32fa3cf17c4b0531
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-04-27 20:13:24 -0400

    Half the time, twice the fun

--- palindrome2.rb
+++ palindrome2.rb
@@ -3,7 +3,10 @@ def is_palindrome(s)
     s = s.gsub(/[^0-9a-z]/i, '')
     arr = s.split('')
     count = arr.count
-    for i in 0..count do
+    half_count = (count  / 2).ceil
+    puts "The count is " + half_count.to_s
+    for i in 0..half_count do
+        puts i
         inverse_num = 0 -1 - i
         if arr[i] != arr[inverse_num]
             return false
