056d01d287dde1439842680600191f32607833c7
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-05-01 19:53:16 -0400

    Begin code portion

--- valid_anagram.rb
+++ valid_anagram.rb
@@ -1,7 +1,11 @@
 def is_anagram(s, t)
     #take all the characters and make an array
+    s = s.split
+    t = t.split # does t.split() do the same thing?
     #sort all the characters
+    s = s.sort
     #sort all the characters in the other array
+    t = t.split
     # loop through a count/2 ceil of the first array and find that spot in each place
     # at the same time, loop through that count from the end
 end
