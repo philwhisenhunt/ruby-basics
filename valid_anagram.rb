6874ae35c5b95c583385690c5cc30ccf5b3bedfb
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-04-30 22:48:21 -0400

    Wrap up plan

--- valid_anagram.rb
+++ valid_anagram.rb
@@ -2,4 +2,6 @@ def is_anagram(s, t)
     #take all the characters and make an array
     #sort all the characters
     #sort all the characters in the other array
+    # loop through a count/2 ceil of the first array and find that spot in each place
+    # at the same time, loop through that count from the end
 end
