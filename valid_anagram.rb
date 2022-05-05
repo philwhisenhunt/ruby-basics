64bafb9cf88c086292fdf0691497ee672174f976
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-05-05 17:24:57 -0400

    Pass the first test

--- valid_anagram.rb
+++ valid_anagram.rb
@@ -1,15 +1,18 @@
 def is_anagram(s, t)
     #take all the characters and make an array
-    s = s.split
-    t = t.split # does t.split() do the same thing?
+ 
+    s = s.split("")
+    t = t.split("")
+     # does t.split() do the same thing?
     #sort all the characters
     s = s.sort
     #sort all the characters in the other array
-    t = t.split
+    t = t.sort
     # loop through a count/2 ceil of the first array and find that spot in each place
     count = s.count
     for i in 0..count do
         if s[i] != t[i]
+            puts s[i] 
             return false
         end
 
@@ -20,3 +23,8 @@ def is_anagram(s, t)
     end
     return true
 end
+
+s = "anagram"
+t = "nagaram"
+
+puts is_anagram(s, t)
