7a4a1df18573b03b16fcb51ada565ddc2245b3de
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-05-07 08:52:34 -0400

    First example passes, fails suite

--- two_sum.rb
+++ two_sum.rb
@@ -1,5 +1,16 @@
 def two_sum(nums, target)
     # for each number, add it to each other number 
     # compare that sum to target
+
+    for i in 0..nums.count do
+        rem = target - nums[i]
+        if nums.include? rem
+            return [i, nums.index(rem)]
+        end
+    end
     
 end
+
+nums = [2,7,11,15]
+target = 9
+print two_sum(nums, target)
