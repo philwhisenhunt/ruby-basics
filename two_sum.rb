62c2fa7c92bb14069e71ed7d4c019d7d4e0adeb6
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-05-07 09:20:42 -0400

    Technically works, but time limit issue

--- two_sum.rb
+++ two_sum.rb
@@ -9,8 +9,8 @@ def two_sum(nums, target)
     minus_one_count = nums.count - 1
     for i in 0..minus_one_count do
      for j in 1..minus_one_count do
-        puts "i:" + i.to_s + " j:" + j.to_s
-        if nums[i] + nums[j] == target
+      
+        if nums[i] + nums[j] == target && (i != j)
             return [i, j]
         end
      end
@@ -21,6 +21,6 @@ end
 # nums = [2,7,11,15]
 # target = 9
 
-nums = [3,2,4]
-target = 6
+nums = [2,5,5,11]
+target = 10
 print two_sum(nums, target)
