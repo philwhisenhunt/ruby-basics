06ee76355b516b5bebd8580ca6edec31dbbf3069
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-05-16 17:07:35 -0400

    wrestling with data types
    What is preventing us from using number_storage as the key?

--- two_sum.rb
+++ two_sum.rb
@@ -24,18 +24,24 @@ def two_sum_hash(nums, target)
     # puts nums.count - 1
     hash = {}
     count = nums.count - 1
-    puts count.to_s + " is the count"
+    # puts count.to_s + " is the count"
     for i in 0..count do
         difference = target - nums[i]
+        puts "Difference: " + difference.to_s
+        puts "and the hash includes it?: " + hash.include?(difference).to_s
         if hash.include?(difference)
-            return true
+            return [nums.index(hash[i]), nums.index(nums.index[i])]
         end
         # puts i
         # hash[i] = nums[i]
-        hash[i] = nums[i]
+        number_storage = nums[i]
+        # hash[number_storage] = i
+        print number_storage
+        hash[number_storage.to_s] = 200
+        print "hash is " + hash.to_s
     end
-    puts "The hash is: "
-    print hash
+    # puts "The hash is: "
+    # print hash
 end
 nums = [2,5,5,11]
 target = 10
