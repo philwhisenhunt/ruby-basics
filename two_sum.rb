a939d3039601fa580e9510dbabca096a13648760
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-05-19 17:32:51 -0400

    Clear out comments, rename

--- two_sum.rb
+++ two_sum.rb
@@ -20,36 +20,19 @@ nums = [2,5,5,11]
 target = 10
 # print two_sum(nums, target)
 
-def two_sum_hash(nums, target)
-    # puts nums.count - 1
+def two_sum(nums, target)
     hash = {}
     count = nums.count - 1
-    # puts count.to_s + " is the count"
     for i in 0..count do
-        # puts i
         difference = target - nums[i]
-        puts difference
-        # puts "Difference: " + difference.to_s
-        # puts "and the hash includes it?: " + hash.include?(difference).to_s
         difference = difference.to_s
-        if difference == "5"
-            puts hash.include?(difference)
-        end
         if hash.include?(difference)
-            # puts "Hash is " + hash[difference].to_s
-            return [hash[difference], nums.index(i)]
+            return [hash[difference], i]
         end
-        # puts i
-        # hash[i] = nums[i]
         number_storage = nums[i]
-        # hash[number_storage] = i
-        # print number_storage
         hash[number_storage.to_s] = i
-        # print "hash is " + hash.to_s
     end
-    # puts "The hash is: "
-    # print hash
 end
 nums = [2,7,11,15]
-target = 19
-print two_sum_hash(nums, target)
+target = 9
+print two_sum(nums, target)
