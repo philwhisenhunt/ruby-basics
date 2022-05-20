60211575093637e21aaa158006aad1dcbbf450f3
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-05-20 19:41:50 -0400

    Plan it out

--- group_anagrams.rb
+++ group_anagrams.rb
@@ -1,4 +1,15 @@
 def group_anagrams(strs)
+    #brute way
+    # for each sort by alphabet
+    # run through and compare
+
+    #alternatively
+    # make a hash
+    # only go through the anagrams once. Sort it, then
+    # see if that sorted version exists in the hash
+    # if it does, put it in the existing group
+    # if not, make it a new group
+
     
 end
 
