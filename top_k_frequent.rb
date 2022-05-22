005723e903c517bb6ee789143203556dd7c0427f
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-05-22 14:34:08 -0400

    Prep plan

--- top_k_frequent.rb
+++ top_k_frequent.rb
@@ -3,5 +3,13 @@ def top_k_frequent(nums, k)
     # return the k most frequent elements. 
     # You may return the answer in any order.
 
+    # create a hash
+    #iterate through array
+    # for each one, check if it is in the hash
+    # if it is, increase the value for that key by one
+    # if not, add it to the hash
     
 end
+nums = [1,1,1,2,2,3]
+k = 2
+top_k_frequent
