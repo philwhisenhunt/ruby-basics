c054e6e6fd7178471d3495f97880fc63409406d3
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-05-23 18:03:28 -0400

    Begin hashing

--- top_k_frequent.rb
+++ top_k_frequent.rb
@@ -4,10 +4,15 @@ def top_k_frequent(nums, k)
     # You may return the answer in any order.
 
     # create a hash
+    hash_it = {}
     #iterate through array
-    # for each one, check if it is in the hash
-    # if it is, increase the value for that key by one
-    # if not, add it to the hash
+    nums.each do |num|
+        if hash.include?(num)
+            hash[num.to_s] = hash[num.to_s] + 1
+        # for each one, check if it is in the hash
+        # if it is, increase the value for that key by one
+        # if not, add it to the hash
+    end
     # sort the hash from greatest to the least
     # return the first two results
     
