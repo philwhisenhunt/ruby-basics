9d552751871113fe8d7c86043de83393b913fa30
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-05-27 19:23:50 -0400

    Flailing

--- top_k_frequent.rb
+++ top_k_frequent.rb
@@ -4,33 +4,15 @@ def top_k_frequent(nums, k)
     # You may return the answer in any order.
 
     # create a hash
-    hash_it = {}
+    count = {}
     frequency = []
 
-    nums.each do |i|
-        puts i
+    nums.each do |n|
+        count["n"] <= 1
     end
 
+    return count
 
-
-    #iterate through array
-    # nums.each do |num|
-    #      # for each one, check if it is in the hash
-    #     # if it is, increase the value for that key by one
-    #     if hash_it.key?(num.to_s)
-            
-    #         hash_it[num.to_s] = hash_it[num.to_s] + 1
-    #     else
-    #         # if not, add it to the hash_it
-    #         hash_it[num.to_s] = 1
-    #     end
-       
-    # end
-
-  
-
-
-    
 end
 nums = [1,1,1,2,2,3]
 k = 2
