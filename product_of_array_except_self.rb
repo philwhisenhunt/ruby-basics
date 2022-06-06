b0df6b02d378fb587cd302a729448b6dc0204e6f
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-06-06 17:23:48 -0400

    Not the correct way to remove an array element

--- product_of_array_except_self.rb
+++ product_of_array_except_self.rb
@@ -1,7 +1,11 @@
 def product_except_self(nums)
+    hash_it = {}
     nums.each do |i|
-        puts i
+        nums = nums.delete(i)
+        hash_it[i] = nums.inject(:*)
+        # puts i
     end
+    print hash_it
 end
 
 nums = [1,2,3,4]
