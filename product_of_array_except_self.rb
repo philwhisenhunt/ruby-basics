f01a31fc9c9ef7dfe6a44e22beeabccfb31bac9d
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-06-09 21:17:48 -0400

    This didn't reassign things they way I expected

--- product_of_array_except_self.rb
+++ product_of_array_except_self.rb
@@ -2,16 +2,20 @@ def product_except_self(nums)
     hash_it = {}
     products = []
     instance_nums = nums
+    hold_array = []
     nums.each do |i|
         # instance_nums = nums - [i]
-        puts i
+        # puts i
+        hold_array << nums.each if 
         # print instance_nums
         instance_nums.delete_at(instance_nums.index(i))
+        # print instance_nums
         # print instance_nums
         # instance_nums = nums
         # hash_it[i] = nums.inject(:*)
         products.append(instance_nums.reject(&:nil?).inject(:*))
-       
+        instance_nums = nums
+        print instance_nums
     end
     # return products
 end
