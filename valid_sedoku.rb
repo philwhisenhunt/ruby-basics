cfe2adce1a4c316e1cad7d97a23b00968777a315
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-06-11 22:41:25 -0400

    Set plan

--- valid_sedoku.rb
+++ valid_sedoku.rb
@@ -1,3 +1,7 @@
 def is_valid_sudoku(board)
-    
+    #check row
+    #check column
+    #check boxes
+        # check first three of first three rows
+        #check second three of first three rows (etc)
 end
