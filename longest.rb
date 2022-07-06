4d19122bfd55124b1ace007d1deadbabeef68b67
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-07-06 10:01:06 -0400

    This way just returns the original count for the example

--- longest.rb
+++ longest.rb
@@ -3,53 +3,18 @@ def longest_consecutive(nums)
     hash_it = {}
     # take each number
     nums.each do |n|
-        hash_it[n] = 1
+        # hash_it[n] = 1
 
-        if nums.include?(n - 1)
+        if nums.include?(n - 1) || nums.include?(n + 1)
+            next
+        else
+            nums.delete(n)
+        end
             
-            hash_it[n - 1] = 1
-        else
-            hash_it[n - 1] = 0
-        end
-
-        if nums.include?(n + 1)
-            hash_it[n + 1] = 1
-        else
-            hash_it[n + 1] = 0
-        end
-
     end
-    puts "hash_it: "
-    puts hash_it
-
-    puts ""
-    puts "hash_it sorted: "
-    print hash_it.keys
-
-    counter = 0
-    high_score = 0
-    # for m in  0 - hash_it.count..hash_it.count
-
-    #     if !hash_it[m]
-    #         next
-    #     end
-
-    #     if hash_it[m] < 1
-    #         counter = 0
-    #         next
-    #     end
-        
-    #     counter += 1
-        
-
-    #     if counter > high_score
-    #         high_score = counter
-    #     end
-        
-    # end
-    return high_score
-    # print hash_it
-    # sort in order
+    print nums
+    return nums.count
+   
 end
 
 nums = [0,3,7,2,5,8,4,6,0,1]
