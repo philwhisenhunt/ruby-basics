19ec49c9d84f82c1035c9dea050140cfc031469e
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-07-06 09:22:27 -0400

    Solve for [0,-1]

--- longest.rb
+++ longest.rb
@@ -19,10 +19,11 @@ def longest_consecutive(nums)
         end
 
     end
+    # puts hash_it
 
     counter = 0
     high_score = 0
-    for m in  0..hash_it.count
+    for m in  0 - hash_it.count..hash_it.count
 
         # puts "hash_it is: " + hash_it[m].to_s
         if !hash_it[m]
@@ -49,4 +50,7 @@ end
 nums = [0,3,7,2,5,8,4,6,0,1]
 nums = [100,4,200,1,3,2]
 nums = [0]
+nums = [0,-1]
+
+[9,1,4,7,3,-1,0,5,8,-1,6]
 print longest_consecutive(nums)
