a0ede41f38eb0553566c0808f0d61a32c3815d7e
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-07-08 20:26:32 -0400

    Add to plan

--- 3sum.rb
+++ 3sum.rb
@@ -1,6 +1,10 @@
 def three_sum(nums)
     # for each number, find the other two that are needed
     # check nums for those numbers
+
+    #for loop the length of the array
+
+    
 end
 
 nums = [-1,0,1,2,-1,-4]
