5e29e88bcb2fbb5039d9df01555a395cc077b3ec
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-07-18 22:12:16 -0400

    Add search point

--- 3sum.rb
+++ 3sum.rb
@@ -5,6 +5,7 @@ def three_sum(nums)
     # for loop the length of the array
     nums.each_with_index do |n, index|
         checkpoint = n + nums[index + 1]
+        searchpoint = checkpoint - 0
     end
     # check the number and the one in the index next to it
     # add the two together, and check the array for 0 minus that number
