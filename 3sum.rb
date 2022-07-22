d7c96fc11c975a1c046f235d475b0d25da2737a5
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-07-21 22:05:17 -0400

    Stash agreeables

--- 3sum.rb
+++ 3sum.rb
@@ -2,12 +2,15 @@ def three_sum(nums)
     # for each number, find the other two that are needed
     # check nums for those numbers
 
+    agreeable_numbers = []
     # for loop the length of the array
     nums.each_with_index do |n, index|
         checkpoint = n + nums[index + 1]
         searchpoint = checkpoint - 0
         if nums.include?(searchpoint)
             # stash all the numbers
+            agreeable_numbers << searchpoint
+            agreeable_numbers << n #fix duplicate here
         end
     end
     # check the number and the one in the index next to it
