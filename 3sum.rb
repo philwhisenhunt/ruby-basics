e73e43b305e5e2fd256be8353773b17ed547a65f
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-08-26 17:56:12 -0400

    Target list aquired

--- 3sum.rb
+++ 3sum.rb
@@ -5,15 +5,17 @@ def three_sum(nums)
     added_count = 0
     no_fly_list = []
     checkpoint_array = []
+    list_of_targets = {}
  
     nums.each_with_index do |n, index|
         target = 0 - n
-
+        list_of_targets[target] = index
         # then search the rest of the array for numbers 
         # that together equal the target
+        # maybe store the target along with its index location?
       
     end
-    return agreeable_numbers.uniq 
+    return list_of_targets
 
     
 end
