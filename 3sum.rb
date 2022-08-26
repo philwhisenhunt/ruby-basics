2205fcaf668855bc147b91fcb5c645447178df11
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2022-08-25 21:45:17 -0400

    Reset after reread

--- 3sum.rb
+++ 3sum.rb
@@ -9,36 +9,7 @@ def three_sum(nums)
     # for loop the length of the array
     nums.each_with_index do |n, index|
         # puts nums[index + 1]
-        if !nums[index + 1].nil? 
-            #this instead needs to check all the other numbers
-            # or we could all of them in, sorted
-            #then remove dupes
-
-            # we need to alter how we search here, not just do the number next to it
-            # better yet, alter the checkpoint, so that each number gets subtracted from zero then added to a new array
-            checkpoint = 0 - n
-            
-            # puts !nums.include?(searchpoint) 
-            if nums.include?(checkpoint) && !no_fly_list.include?(index) 
-                # puts searchpoint
-                # stash all the numbers
-
-                # this needs to include the third number somehow
-                agreeable_numbers << [checkpoint, n].sort
-                
-                no_fly_list << index
-                # no_fly_list << searchpoint
-                # agreeable_numbers << n #fix duplicate here
-                # or make agreeable_numbers uniq here
-                
-            end
-            checkpoint_array << checkpoint
-            # consider adding a nofly list to skip numbers
-            # if !no_fly_list.include?(n)
-          
-            #make a new final array here
-            #or cycle through array and export them three at a time
-        end
+      
     end
     return agreeable_numbers.uniq 
     # agreeable_numbers
