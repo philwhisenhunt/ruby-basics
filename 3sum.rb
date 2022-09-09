6014b0b551f847a7a7edf5119297cec3ee541221
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-09-08 21:08:58 -0400

    Another progress attempt

--- 3sum.rb
+++ 3sum.rb
@@ -1,9 +1,25 @@
 def three_sum(nums)
  
 
+first = 0
+last = nums.count - 1
+triplet = []
+final_array = []
+sum = nil
 
+while (first < last) do
+    sum = nums[first] + nums[last]
+    if nums.include?(sum)
+        triplet << [sum, nums[first], nums[last]]
+        final_array << triplet
+        break
+    else
+        # how to get rid of the rest of them?
+    end
 
-    
+end
+
+return final_array
 end
 
 nums = [-1,0,1,2,-1,-4]
