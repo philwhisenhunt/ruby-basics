0887ecebb4017fa4f43f8ab886340eb214fb8353
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-09-21 18:43:25 -0400

    Set plan

--- max_profit.rb
+++ max_profit.rb
@@ -1,5 +1,9 @@
 def max_profit(prices)
-    
+    # for everything in the prices array
+    # set the first one as the high and the low
+    # for each one after, check if it is higher or lower
+    # if lower, change lowest
+    # if higher, change highest
 end
     
 prices = [7,1,5,3,6,4]
