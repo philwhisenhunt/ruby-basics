4aa0783f3a3499a3a6f56f53f893ae0570fe2c65
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-09-20 20:47:42 -0400

    Thought it matched, but empty array

--- 3sum.rb
+++ 3sum.rb
@@ -2,17 +2,52 @@ def three_sum(nums)
  
 res = []
 nums.sort()
-nums.each do |num, index|
-    if num > 0 && num == nums[i - 1]
-        l = i + 1
+nums.each_with_index do |num, index|
+    if num > 0 && num == nums[index - 1]
+        next
+    end
+        l = index + 1
         r = nums.length - 1
 
         while l < r
-            threeSum = a + nums[l] + nums[r]
-        end
-    end
-end
 
+            print "l is " 
+            puts l
+
+            print "r is " 
+            puts r
+            print "Num is: "
+            puts num
+
+            print "Num[l] is: "
+            puts num[l]
+
+            
+            print "Num[r] is: "
+            puts num[r]
+
+
+            threeSum = num + nums[l] + nums[r]
+            puts threeSum
+
+            if threeSum > 0
+                r -= 1
+            elsif threeSum < 0
+                l += 1
+            else
+                puts "yes"
+                res << [num, nums[l], nums[r]]
+                l += 1
+                while nums[l] == nums[l - 1] && l < r
+                    l+= 1
+                end
+            end
+
+        end
+   
+    # puts num
+end
+return res
 end
 
 nums = [-1,0,1,2,-1,-4]
