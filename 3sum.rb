c331b92e75328b46009714ec0a516e444651fa75
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-09-20 20:55:57 -0400

    Final, correct version

--- 3sum.rb
+++ 3sum.rb
@@ -2,55 +2,35 @@ def three_sum(nums)
  
 res = []
 nums = nums.sort()
-# print nums
+
 nums.each_with_index do |num, index|
     if index > 0 && num == nums[index - 1]
         next
     end
-        l = index + 1
-        r = nums.length - 1
+    l = index + 1
+    r = nums.length - 1
+    while l < r
 
-        while l < r
+        threeSum = num + nums[l] + nums[r]
 
-            # print "l is " 
-            # puts l
-
-            # print "r is " 
-            # puts r
-            # print "Num is: "
-            # puts num
-
-            # print "Num[l] is: "
-            # puts num[l]
-
-            
-            # print "Num[r] is: "
-            # puts num[r]
-
-
-            threeSum = num + nums[l] + nums[r]
-            # puts threeSum
-
-            if threeSum > 0
-                r -= 1
-            elsif threeSum < 0
-                l += 1
-            else
-                puts "yes"
-                res << [num, nums[l], nums[r]]
-                l += 1
-                while nums[l] == nums[l - 1] && l < r
-                    l+= 1
-                end
+        if threeSum > 0
+            r -= 1
+        elsif threeSum < 0
+            l += 1
+        else
+            puts "yes"
+            res << [num, nums[l], nums[r]]
+            l += 1
+            while nums[l] == nums[l - 1] && l < r
+                l+= 1
             end
-
         end
-   
-    # print res
+
+    end
 end
 return res
 end
 
 nums = [-1,0,1,2,-1,-4]
-# nums = [-4, -1, -1, 0, 1, 2]
+
 print three_sum(nums)
