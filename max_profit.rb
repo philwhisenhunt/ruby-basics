db176ef7575dd68b05ec4ef137d42fe12aaa4163
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-09-22 21:37:42 -0400

    Begin making the loop

--- max_profit.rb
+++ max_profit.rb
@@ -1,9 +1,12 @@
 def max_profit(prices)
     # for everything in the prices array
+    prices.each_with_index do [price, index]
+
     # set the first one as the high and the low
     # for each one after, check if it is higher or lower
     # if lower, change lowest
     # if higher, change highest
+    end
 end
     
 prices = [7,1,5,3,6,4]
