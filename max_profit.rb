5df5860079faf0ff7e13d32200b5d27e79b5eabb
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-09-23 22:32:06 -0400

    Initialize first and last

--- max_profit.rb
+++ max_profit.rb
@@ -1,9 +1,17 @@
 def max_profit(prices)
     # for everything in the prices array
+    high = nil
+    low = nil
     prices.each_with_index do [price, index]
 
     # set the first one as the high and the low
+    if index == 0
+        high = prices[index] 
+        low = prices[prices.length - 1]
+    end
+
     # for each one after, check if it is higher or lower
+    
     # if lower, change lowest
     # if higher, change highest
     end
