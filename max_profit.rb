16ec90a4e9e2d962f9f4439dacede7c72231a997
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-09-26 16:57:30 -0400

    Clean up

--- max_profit.rb
+++ max_profit.rb
@@ -14,32 +14,19 @@ def max_profit(prices)
             low_location = index
             puts "low_location is " + low_location.to_s
         end
-        # puts index
-        # # unless index >= high_location
+      
         if price > high
-            # puts "hup"
-            # puts high_location
-            # puts index
             if index > high_location
                 high = price
             end
         elsif price < low
-            # puts "help"
-            # puts "Index is " + index.to_s
-            # puts "and now low_location is " + low_location.to_s
-            # puts "lup"
+
             if index < low_location
                 low = price
                 low_location = index
             end
         end
-        # puts high
-        # puts low
 
-        # for each one after, check if it is higher or lower
-        
-        # if lower, change lowest
-        # if higher, change highest
     end
     return high - low
 end
