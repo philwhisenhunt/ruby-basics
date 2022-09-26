6030dfe6d9b8fa330202977309b9f01f429fd0f1
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-09-26 16:56:53 -0400

    Document emerging messy attempt

--- max_profit.rb
+++ max_profit.rb
@@ -2,25 +2,45 @@ def max_profit(prices)
     # for everything in the prices array
     high = nil
     low = nil
+    high_location = nil
+    low_location = nil
     prices.each_with_index do |price, index|
 
         # set the first one as the high and the low
         if index == 0
             high = prices[index] 
             low = prices[prices.length - 1]
+            high_location = index
+            low_location = index
+            puts "low_location is " + low_location.to_s
         end
-
+        # puts index
+        # # unless index >= high_location
         if price > high
-            high = price
+            # puts "hup"
+            # puts high_location
+            # puts index
+            if index > high_location
+                high = price
+            end
         elsif price < low
-            low = price
+            # puts "help"
+            # puts "Index is " + index.to_s
+            # puts "and now low_location is " + low_location.to_s
+            # puts "lup"
+            if index < low_location
+                low = price
+                low_location = index
+            end
         end
+        # puts high
+        # puts low
 
         # for each one after, check if it is higher or lower
         
         # if lower, change lowest
         # if higher, change highest
-        end
+    end
     return high - low
 end
     
