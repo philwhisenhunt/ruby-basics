683e582eb23f4c9c0706f6727ad726bf0e527fff
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-09-25 21:39:14 -0400

    Initial version that returns the high and the low

--- max_profit.rb
+++ max_profit.rb
@@ -2,19 +2,27 @@ def max_profit(prices)
     # for everything in the prices array
     high = nil
     low = nil
-    prices.each_with_index do [price, index]
+    prices.each_with_index do |price, index|
 
-    # set the first one as the high and the low
-    if index == 0
-        high = prices[index] 
-        low = prices[prices.length - 1]
-    end
+        # set the first one as the high and the low
+        if index == 0
+            high = prices[index] 
+            low = prices[prices.length - 1]
+        end
 
-    # for each one after, check if it is higher or lower
-    
-    # if lower, change lowest
-    # if higher, change highest
-    end
+        if price > high
+            high = price
+        elsif price < low
+            low = price
+        end
+
+        # for each one after, check if it is higher or lower
+        
+        # if lower, change lowest
+        # if higher, change highest
+        end
+    return [low, high]
 end
     
 prices = [7,1,5,3,6,4]
+print  max_profit(prices)
