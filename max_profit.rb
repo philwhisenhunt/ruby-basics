79a899629b0972560584ba0ee9590f1d9354ffe6
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-09-30 20:43:13 -0400

    Clean up to past leetcode time requirements

--- max_profit.rb
+++ max_profit.rb
@@ -1,14 +1,12 @@
 def max_profit(prices)
-    # for everything in the prices array
+
     max_profit = 0
-    # profit = 0
+   
     l = 0
     r = 1
-
     puts prices.length
     while r < (prices.length) do
-        puts "r is " + r.to_s
-        puts "l is " + l.to_s
+
         if prices[l] < prices[r]
             profit = prices[r] - prices[l]
             max_profit = [max_profit, profit].max()
@@ -17,8 +15,7 @@ def max_profit(prices)
             l = r
            
         end
-        r += 1
-       
+        r += 1 
     end
     return max_profit
 end
