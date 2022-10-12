5bd2ec903408ad4274a846e10214512ebfb56e6a
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-10-12 19:22:59 -0400

    Currently stuck

--- size_three.rb
+++ size_three.rb
@@ -9,28 +9,24 @@ def count_good_substrings(s)
   
 
    while r < (s.length) do
-        if s[r] == s[r - 1]
-            l += 1
-            r = l + 1
-        elsif (r - l) == 2 && s[l] == s[r]
-            good_substrings_count += 1
-            l += 1
-            r += 1
-        elsif (r - l) > 2
-            l += 1
-        else
-            r += 1
-        end
-        puts "l is " + l.to_s
-        puts "r is " + r.to_s
-        puts " --"
+
+    if s[l] == s[r]
+        l += 1
+    end
+
+    if (r - l) == 2
+        good_substrings_count += 1
+        l += 1
+    end
+
+    r += 1
    
     end
     return good_substrings_count
 end
 
 s = "xyzzaz"
-s = "aababcabc"
+# s = "aababcabc"
 
 
 puts count_good_substrings(s)
