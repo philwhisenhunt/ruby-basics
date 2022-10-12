d40003b86ef460574b7897fba9d56a28a4c303ee
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-10-12 19:36:21 -0400

    Trying to see output

--- size_three.rb
+++ size_three.rb
@@ -10,17 +10,27 @@ def count_good_substrings(s)
 
    while r < (s.length) do
 
+  
     if s[l] == s[r]
         l += 1
     end
-
-    if (r - l) == 2
+    
+    if (r - l) == 2 && (s[l] != s[r])
+        puts "hit it"
+        puts "l is " + l.to_s
+        puts "r is " + r.to_s
+        puts s[l] + " to " + s[r]
+        puts " --"
         good_substrings_count += 1
         l += 1
+        
     end
+
+   
 
     r += 1
    
+    
     end
     return good_substrings_count
 end
