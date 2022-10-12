3e57b1f32ac9d0c75a184b2e2ee0bba7aeb20327
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-10-12 18:43:04 -0400

    This reduced the correct count

--- size_three.rb
+++ size_three.rb
@@ -8,9 +8,22 @@ def count_good_substrings(s)
    r = 1
   
 
-   while r < s.length do
-    if s[r] == s[r - 1]
-        l += 1
+   while r < (s.length) do
+    puts "r is " + r.to_s
+    puts "l is " + l.to_s
+    puts "---"
+        if s[r] == s[r - 1]
+            l += 1
+        end
+
+        if r - l == 2 && s[l] != s[r]
+            good_substrings_count += 1
+            l += 1
+        end
+        r += 1
     end
-    r += 1
+    return good_substrings_count
 end
+
+s = "xyzzaz"
+puts count_good_substrings(s)
