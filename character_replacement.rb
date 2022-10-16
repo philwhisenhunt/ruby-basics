2a6368ce899080b96a16aabd1b3de0d0e7bfb076
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-10-16 12:32:06 -0400

    Set up testing

--- character_replacement.rb
+++ character_replacement.rb
@@ -1,5 +1,15 @@
 
 def character_replacement(s, k)
     #replace pieces of s with capitalized versions k times
-    while r < s.length
+    l = 0
+    r = 0
+    while r < s.length do 
+        puts r
+
+        r += 1
+    end
 end
+
+s = "ABAB"
+k = 2
+character_replacement(s, k)
