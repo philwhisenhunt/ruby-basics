32a2273aa70e221e2374d0c16afaf346e3cdf8ca
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-10-17 21:50:12 -0400

    Add in used_array

--- character_replacement.rb
+++ character_replacement.rb
@@ -3,8 +3,13 @@ def character_replacement(s, k)
     #replace pieces of s with capitalized versions k times
     l = 0
     r = 0
+    used_array = {}
     while r < s.length do 
         puts r
+
+        if used_array.include?(s[r])
+            return
+        end
 
         r += 1
     end
