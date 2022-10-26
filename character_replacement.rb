9c43b51124a3ca52dbfaa2ce4c4e17899cc0cfe8
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-10-25 20:16:44 -0400

    Add count_of for tracking

--- character_replacement.rb
+++ character_replacement.rb
@@ -3,15 +3,17 @@ def character_replacement(ss, k)
     #replace pieces of s with capitalized versions k times
     l = 0
     r = 0
-    used_array = {}
+    count_of = 0
+    # used_array = {}
     while r < ss.length do 
        # now we need to look for a gap in repitition that is K long?
         if r != l
             #then we can increase the count by one and keep going
+            count_of += 1
         end
-        if used_array.include?(ss[r])
-            break
-        end
+        # if used_array.include?(ss[r])
+        #     break
+        # end
 
         used_array[ss[r]] = ss[r].downcase if ss[r] == ss[r].upcase
 
