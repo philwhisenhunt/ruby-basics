c64b01ce4b1c2a81f45657680627bd963dff43c5
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-10-26 21:23:24 -0400

    Handle cases, fix loop

--- character_replacement.rb
+++ character_replacement.rb
@@ -4,12 +4,17 @@ def character_replacement(ss, k)
     l = 0
     r = 0
     count_of = 0
+    flipped_array = []
     # used_array = {}
     while r < ss.length do 
        # now we need to look for a gap in repitition that is K long?
-        if r != l
+        if ss[r] != ss[l]
+
+            reverse_cased = ss[r] == ss[r].upcase ? ss[r].downcase : ss[r].upcase
+            flipped array << reverse_cased
             #then we can increase the count by one and keep going
             count_of += 1
+
         end
         # if used_array.include?(ss[r])
         #     break
