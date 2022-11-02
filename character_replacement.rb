1cfd609ae1e99db01ee39ee0a4479842cf91caf0
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-11-02 17:15:02 -0400

    Planning

--- character_replacement.rb
+++ character_replacement.rb
@@ -7,7 +7,10 @@ def character_replacement(ss, k)
     flipped_array = []
     used_array = {}
     while r < ss.length do 
-
+        # Loop through once
+        # For each letter, see how long ago the same letter was
+        # If that number is less than or equal to 2, save it to the counting array
+        # At the end pick the highest number
     end
 
     print used_array.count
