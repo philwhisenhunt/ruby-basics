d8861062068168fb21ede717dcc1be14f4a0c4df
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-11-17 20:07:48 -0500

    Debugging

--- character_replacement.rb
+++ character_replacement.rb
@@ -11,18 +11,29 @@ def character_replacement(ss, k)
     
         count[ss[r]] = !count[ss[r]].nil? ? (1 + count[ss[r]]) : 0 
 
+        puts "-------"
+        print count
+        puts ""
+
+      
+        print count[ss[r]]
+        puts ""
+        puts "-------"
+        puts ""
+        puts ""
         if ((r - 1 + 1) - count.values.max) > k
             count[ss[l]] -= 1
             l += 1
 
         end
 
-
+        
         res = [res, (r - l + 1)].max
+        # puts res
         r += 1
     end
 
-   
+   puts ""
     return res
 end
 
@@ -32,3 +43,6 @@ ss = "ABAB"
 k = 2
 character_replacement(ss, k)
 
+ss = "ABAA"
+k = 0
+character_replacement(ss, k)
