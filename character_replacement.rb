6d2481eb9f360ae04d6574cc2ee4251e58283cb5
Phil Whisenhunt <phil.whisenhunt@slope.io>
2022-11-17 19:52:18 -0500

    Passed first one, failed on "ABAA"

--- character_replacement.rb
+++ character_replacement.rb
@@ -8,21 +8,22 @@ def character_replacement(ss, k)
     r = 0
 
     while r < ss.length do 
+    
         count[ss[r]] = !count[ss[r]].nil? ? (1 + count[ss[r]]) : 0 
 
-        if (r - 1 + 1) - count.values.max > k
+        if ((r - 1 + 1) - count.values.max) > k
             count[ss[l]] -= 1
             l += 1
 
         end
 
 
-        res = [res, r - 1 + 1].max
+        res = [res, (r - l + 1)].max
         r += 1
     end
 
    
-    print res
+    return res
 end
 
 
@@ -30,3 +31,4 @@ end
 ss = "ABAB"
 k = 2
 character_replacement(ss, k)
+
